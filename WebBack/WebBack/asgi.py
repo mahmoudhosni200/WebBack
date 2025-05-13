004872ca0e80589c63616f1f6a8db90f5203275f
mahmoudhosni200 <20230370@stud.fci-cu.edu.eg>
2025-05-13 12:47:14 +0300

    initial commit

--- WebBack/WebBack/asgi.py
+++ WebBack/WebBack/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for WebBackApp project.
+ASGI config for WebBack project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebBackApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebBack.settings')
 
 application = get_asgi_application()
--- WebBack/WebBack/wsgi.py
+++ WebBack/WebBack/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WebBackApp project.
+WSGI config for WebBack project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebBackApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebBack.settings')
 
 application = get_wsgi_application()
