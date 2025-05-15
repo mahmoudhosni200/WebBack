65788f57e566c684494b33dcd22c544148daf668
NayeraShaaban <160131373+NayeraShaaban@users.noreply.github.com>
2025-05-15 18:26:52 +0300

    Delete WebBack/Books/urls.py

--- WebBack/Books/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path('AddBook/', views.addBook, name='addBook'),
-    path('ListAdmin/', views.list_books_admin, name='list_books_admin'),
-    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
-    path('Edit_Book/<int:book_id>/', views.edit_book, name='edit_book'),
-]
