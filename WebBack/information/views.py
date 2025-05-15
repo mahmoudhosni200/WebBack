745cb5ba5fc2d32176d7104565fa4eef913ea27b
MohamedAshraf <mohamed4ashraf2019@gmail.com>
2025-05-15 18:27:35 +0300

    edit

--- WebBack/information/views.py
+++ WebBack/information/views.py
@@ -9,17 +9,17 @@ from .models import Member
 def admin_profile(request):
     return render(request, 'Books/profileAdmin.html', {'admin': request.user})
 
-# def user_profile(request):
-#     return render(request, 'Books/profileUser.html', {'user': request.user})
-from django.shortcuts import render
-from information.models import Member
-
 def user_profile(request):
-    # dummy example
-    member = Member(
-        name='Jane Doe',
-        email='jane@example.com',
-        # profile_picture='image/boy.jpg'
-    )
-    return render(request, 'Books/profileUser.html', {'member': member})
+        return render(request, 'Books/profileUser.html', {'member': request.user})
+
+
+# for testing
+# def user_profile(request):
+#     # dummy example
+#     member = Member(
+#         name='Jane Doe',
+#         email='jane@example.com',
+#         # profile_picture='image/boy.jpg'
+#     )
+#     return render(request, 'Books/profileUser.html', {'member': member})
 
