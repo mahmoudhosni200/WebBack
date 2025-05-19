1f9389b611644d3ed4037d1109e0fc3f76191e71
Anoud <anoudmohammed652gmail.com>
2025-05-19 16:18:12 +0300

    Updates Contact for both Admin & user

--- WebBack/information/models.py
+++ WebBack/information/models.py
@@ -56,3 +56,11 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.email}"    
+class admin_contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"    
